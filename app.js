344453e4fda201458542a7f65c05c9488168367f
chanjong Kim <a3612@naver.com>
2023-10-21 15:05:59 +0900

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const saveBtn = document.getElementById("saveimage");
+const textInput = document.getElementById("text");
+const fileInput = document.getElementById("file");
 const modeBtn = document.getElementById("mode-btn");
 const destroyBtn = document.getElementById("destroy-btn");
 const eraserBtn = document.getElementById("eraser-btn");
@@ -6,14 +9,13 @@ const color = document.getElementById("color");
 const lineWidth =document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 800;
 
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
-
 ctx.linewidth = lineWidth.value;
+ctx.lineCap = "round";
 let isPainting = false;
 let isFilling = false;
 
@@ -23,7 +25,6 @@ function onMove(event){
         ctx.stroke();
         return;
     }
-    ctx.beginPath();
     ctx.moveTo(event.offsetX, event.offsetY);
 }
 
@@ -33,6 +34,7 @@ function startPainting(){
 
 function cancelPainting(){
     isPainting = false;
+    ctx.beginPath();
 }
 
 function onLineWidthChange(event){
@@ -54,29 +56,63 @@ function onColorClick(event){
 function onModeClick(){
     if(isFilling){
         isFilling = false
-        modeBtn.innerText = "Fill"
+        modeBtn.innerText = "Fill🎨"
     } else{isFilling = true 
-        modeBtn.innerText = "Draw"};
-}
+        modeBtn.innerText = "Stroke🎨"};
+    }
+
 
 function onCanvasClick(){
     if(isFilling){
         ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    };
+    }
 }
 
 function onDestroyClick(){
+    if(confirm("Are you sure to Reset Page?")){
     ctx.fillStyle = "white";
     ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+    } else{alert("Keep Drawing!")};
+    
 }
 
 function onEraserClick(){
-    ctx.strokeStyle = "white";
     isFilling = false;
-    modeBtn.innerText = "Fill";
+    ctx.strokeStyle = "white";
+    modeBtn.innerText = "Fill🎨";
 }
 
+function onFileChange(event){
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    const image = new Image();
+    image.src = url;
+    image.onload = function(){
+        ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        fileInput.value = null;
+    }
+}
 
+function onDoubleClick(event){
+    const text = textInput.value;
+    if(text !==""){
+    ctx.save();
+    ctx.lineWidth = 1;
+    ctx.font = "68px serif";
+    ctx.fillText(text, event.offsetX, event.offsetY);
+    ctx.restore();
+}
+}
+
+function onSaveClick(){
+    const url = canvas.toDataURL();
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "my Drawing.png";
+    a.click();
+}
+
+canvas.addEventListener("dblclick", onDoubleClick);
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -88,3 +124,5 @@ colorOptions.forEach(color => color.addEventListener("click", onColorClick));
 modeBtn.addEventListener("click", onModeClick);
 destroyBtn.addEventListener("click", onDestroyClick);
 eraserBtn.addEventListener("click", onEraserClick);
+fileInput.addEventListener("change", onFileChange);
+saveBtn.addEventListener("click", onSaveClick);
